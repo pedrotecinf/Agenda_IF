5eb7b32165b6fda8539a5fd291758025ef950924
pedrotecinf <pedro.augusto.tecinf@gmail.com>
2016-04-13 18:22:19 -0400

    Initial commit

---
+++ app/src/main/java/com/example/pedro_ti/agenda_if/tela_inicio.java
@@ -0,0 +1,13 @@
+package com.example.pedro_ti.agenda_if;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class tela_inicio extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tela_inicio);
+    }
+}
